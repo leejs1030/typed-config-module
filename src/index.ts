608736be5715628b4e916132381c2a973b4923a1
leejs1030 <leejs1030@korea.ac.kr>
2023-08-06 16:08:57 +0900

    export needed components

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { TypedConfigService } from './services/typed-config.service';
+export { initializeTypedConfigModule } from './modules/initialize-typed-config-module';
+export { TypedConfigModuleOptions } from './types/typed-config-module-options';
+export { ResolveJoiSchema } from './types/resolve-joi-schema';
